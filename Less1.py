0a74b46f1a7fbc67ceb8469cb9c872021ade636f
Андрей <andrei.horyngmail.com@MacBook-Pro-Andrej.local>
2022-03-21 00:22:58 +0200

    Добавил словарь

--- Less1.py
+++ Less1.py
@@ -19,3 +19,16 @@ message3 = 'Вам {} лет, ваш рост {} см!' .format(age3,height3)
 print(message3)
 '''
 
+'''
+#Заполение словаря
+user_info = {
+    'first_name': '',
+    'last_name': ''
+}
+user_info['first_name'] = input('Введите ваше имя: ')
+user_info['last_name'] = input('Введите вашу фамилию: ')
+print('Ваше имя - ' + user_info.get('first_name'))
+print('Ваша фамилия - ' + user_info.get('last_name'))
+print('Ваш номер - ' + user_info.get('tel_number', 'Данные не получены'))
+
+'''
